870b8a189731eee352ab04d8a4929b2b8ce0b571
imk <imk@IBRAHIMQ>
2025-08-10 21:22:56 +0300

    Add project files.

---
+++ Ibrahim_MVC_Frist/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ibrahim_MVC_Frist.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
